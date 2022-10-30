edb0f1165ef2c2e48b8e1a369c0cc534033e0c87
Joaquin Kaplan <joaqkaplan@gmail.com>
2022-10-30 19:16:38 -0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import About from './About/About'
+import Contact from './Contact/Contact'
+import Dashboard from './Dashboard/Dashboard'
+import Home from './Home/Home'
+import Layout from './Layout/Layout'
+import Portfolio from './Portfolio/Portfolio'
+import Sidebar from './Sidebar/Sidebar'
+
+export { About, Contact, Dashboard, Home, Layout, Portfolio, Sidebar }
